3d732d6f2763cec6e8ea3580b0d0ba122252c72d
Christopher Huynh <christopher.huynh.is@gmail.com>
2016-12-08 13:07:55 -0800

    add questions to vampires in ruby

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+print "What is your name?\n"
+interviewer_name = gets.chomp
+print "How old are you?\n"
+age = gets.chomp
+print "What year were you born\n?"
+dob = gets.chomp
+print "Our company cafeteria serves garlic bread. Should we order some for you?\n"
+garlic_question = gets.chomp
+print "Would you like to enroll in the company's health insurance?\n"
+health_insurance = gets.chomp
