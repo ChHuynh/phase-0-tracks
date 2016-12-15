430b775649eab274a3467d0bf0a436005a78b149
Christopher Huynh <christopher.huynh.is@gmail.com>
2016-12-15 11:48:56 -0800

    add rpg classes to nested_data_structures

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,39 @@
+adventurer = {
+	warrior: {
+		job: "Warrior",
+		primary_attribute: "trength",
+		equipment: [
+			"sword",
+			"shield",
+			"heavy armor"
+		]
+	}
+	Mage: {
+		job: "Mage",
+		primary_attribute: "intelligence",
+		equipment: [
+			"staff",
+			"grimoires",
+			"robes",
+		]
+	}
+	thief: {
+		job: "Thief",
+		primary_attribute: "dexterity",
+		equipment:[
+			"daggers",
+			"bow",
+			"leather armor"
+		]
+	}
+	priest:{
+		job: "Priest",
+		primary_attribute: "faith"
+		equipment:[
+			"staff",
+			"talisman",
+			"holy robes"
+		]
+	}
+
+}
