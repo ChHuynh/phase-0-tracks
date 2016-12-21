47f48bf67cf162ac831a4b77881bd287290ac587
Christopher Huynh <christopher.huynh.is@gmail.com>
2016-12-21 10:36:08 -0800

    add things a puppy class might contain

---
+++ ruby/puppy.rb
@@ -0,0 +1,5 @@
+# puppies eat food
+# puppies bark
+# puppies jump around
+# maybe taught to: roll over, play dead, fetch things.
+#characteristics: cute, fluffy, soft, can have different fur colors/textures
